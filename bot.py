7460a11d5380a8d53956d985e5bc81bf7db596ef
TimmontCd <74442827+TimmontCd@users.noreply.github.com>
2025-05-27 21:38:55 -0600

    test

--- bot.py
+++ bot.py
@@ -11,7 +11,5 @@ def home():
 def track():
     now = datetime.now()
     user_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
-    print(f"[{now.strftime("%Y-%m-%d %H:%M:%S")}] IP registrada: {user_ip}")
-    return redirect("https://www.facebook.com")  # Cambia el enlace por donde quieras redirigir
-
-app.run(host="0.0.0.0", port=3000)
+    print(f"[{now.strftime('%Y-%m-%d %H:%M:%S')}] IP registrada: {user_ip}")
+    return redirect("https://www.facebook.com")  # Redirige a donde quieras
